0a1902e1feec9956916e148d6acc573ca84adc23
SethminiDissanayake <sethminidissa@gmail.com>
2024-02-01 17:42:14 +0530

    Create CustomerDTO class

---
+++ src/main/java/lk/ijse/gese66/dto/CustomerDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.gese66.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class CustomerDTO {
+    private String id;
+    private String name;
+    private String address;
+}
