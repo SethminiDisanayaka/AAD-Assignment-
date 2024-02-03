ad970c52d3b4855018267ff150c75a889619621c
SethminiDissanayake <sethminidissa@gmail.com>
2024-02-03 21:11:25 +0530

    Database connection pool is created

--- dbcp/src/main/java/lk/ijse/gdse66/dpcp/CustomerServlet.java
+++ dbcp/src/main/java/lk/ijse/gdse66/dpcp/CustomerServlet.java
@@ -1,4 +1,30 @@
 package lk.ijse.gdse66.dpcp;
 
-public class CustomerServlet extends {
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet(name = "Customer", value = "/customer")
+public class CustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext sc = getServletContext();
+        BasicDataSource dbcp = (BasicDataSource) sc.getAttribute("dbcp");
+
+        try(Connection connection =dbcp.getConnection()) {
+            System.out.println(connection);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 }
