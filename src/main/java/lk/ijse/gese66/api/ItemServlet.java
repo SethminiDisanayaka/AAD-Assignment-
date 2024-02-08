26821bb4f9cad9c2735d469cd99a280781517018
SethminiDissanayake <sethminidissa@gmail.com>
2024-02-08 19:53:50 +0530

    Item Servlet is created

---
+++ src/main/java/lk/ijse/gese66/api/ItemServlet.java
@@ -0,0 +1,10 @@
+package lk.ijse.gese66.api;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "ItemServlet" ,urlPatterns = "items" , loadOnStartup = 1)
+public class ItemServlet extends HttpServlet {
+
+
+}
